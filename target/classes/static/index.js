5b0e9b985d710ccd63f71cb13a1e072e911d75e5
Gintautas S <dihitales@gmail.com>
2022-05-03 13:06:46 +0300

    Atsijungimas su AJAX užklausa.

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,9 +1,16 @@
 $(() => {
 	$('a.trinti').click(deleteHandler);
+	$('a#logout_link').click(logoutHandler);
 });
 
 function deleteHandler() {
 	$.post($(this).attr('href'), { id: $(this).attr('data-id') }).
 		done(() => { $(this).parents('.irasas').remove() });
+	return false;
+}
+
+function logoutHandler() {
+	$.post($(this).attr('href'), { _csrf: $(this).data('token') }).
+		done((result) => { document.write(result); document.close(); });
 	return false;
 }
